f90c4028119845eecbfbd04f74de1af559af95cc
richardJG <64484661+richardJG@users.noreply.github.com>
2020-11-07 18:21:58 +0000

    Add files via upload
    Initial Commit

---
+++ HeaterControl.ino
@@ -0,0 +1,236 @@
+#include "OTA.h"
+
+
+const long gmtOffset_sec = 0;
+const int daylightOffset_sec = 3600;
+
+// Initialize Telegram BOT
+//String BOTtoken = "1318988101:AAGbLF4R4ComYrneRV4Pn82rV1tKCRgiwG0";  // your Bot Token (Get from Botfather)
+String BOTtoken = "1209654382:AAGHbjC9PmeRgkyaCKdh4E43BzkVt9KvB3g";
+
+// Use @myidbot to find out the chat ID of an individual or a group
+// Also note that you need to click "start" on a bot before it can
+// message you
+String CHAT_ID = "1086222001";
+
+
+WiFiClientSecure clientTCP;
+UniversalTelegramBot bot(BOTtoken, clientTCP);
+
+// working variables  
+uint8_t hmode;                          // 0=OFF 1 = ON  2 = AUTO
+bool hstate;                            // true if heat on
+uint8_t htemp[4];                       // temps for each zone
+uint16_t hzone[3];                      // boundaries for each zone
+bool dst;                               // add one hour if true
+float airTemp;                          // current air temp
+uint8_t hysT;                           // set hysteresis around switch on/off temps
+unsigned long onTime, offTime, totTime; // variables used to record heater on time
+bool logging = false;                   // o/p log messages if true
+uint8_t pRestart;                         // = 1 if restart from ping failure, 0 otherwise
+time_t lastReset; 
+
+
+// EEPROM key variable storage, holds data during power off
+// as long as alterations are saved 
+#define eehmode  0
+#define eehtemp  eehmode + sizeof(uint8_t)
+#define eehzone  eehtemp + sizeof(htemp)
+#define eedst  eehzone + sizeof(hzone)
+#define eehyst eedst + sizeof(dst)
+
+// storage for variables after ping reset
+#define teehmode  eehyst + sizeof(dst)
+#define teehtemp  teehmode + sizeof(hmode)
+#define teehzone  teehtemp + sizeof(htemp)
+#define teedst  teehzone + sizeof(hzone)
+#define teehyst teedst + sizeof(dst)
+#define teetotTime teehyst + sizeof(totTime)
+#define teepRestart teetotTime + sizeof(pRestart)
+#define teelastReset teepRestart + sizeof(lastReset)
+
+
+#define TEMP_TIME 10
+#define TELEGRAM_TIME 1000
+
+
+const int HEATER =  4;    // gpio pin 4 controls heater triac
+bool HEATER_ON = LOW;     // reverse logic 0 switches heater on
+bool HEATER_OFF = HIGH;
+
+// GPIO where the DS18B20 is connected to
+const int oneWireBus = 16;     
+// Setup a oneWire instance to communicate with any OneWire devices
+OneWire oneWire(oneWireBus);
+// Pass our oneWire reference to Dallas Temperature sensor 
+DallasTemperature sensors(&oneWire);
+
+// timer for regular clock sync
+unsigned long lastday;
+
+// variables to set 10sec and 1sec timers
+unsigned long last10;
+unsigned long last1;
+
+unsigned long boostTime;
+uint8_t lastmode;
+
+
+// START OF PROGRAM ********
+#include "heaterlogic.h"
+
+
+ void setup() {
+  Serial.begin(115200);
+  Serial.println("Booting");
+  setupOTA("HeaterControl");
+
+  // for a power on reset get all variables from EEPROM
+  // otherwise variables have been stored in RTC memory
+  // and should be still OK after software restart
+  syncTime();
+  
+  EEPROM.begin(512);
+  EEPROM.get(teepRestart, pRestart);
+  
+  if( pRestart == 0){
+    Serial.println("Power on reset");
+    // power on reset
+    EEPROM.get(eehmode, hmode);
+    EEPROM.get(eehtemp, htemp);
+    EEPROM.get(eehzone, hzone);
+    EEPROM.get(eedst, dst);
+    EEPROM.get(eehyst, hysT);
+    totTime = 0;
+    logging = false;
+    lastReset = now();
+    
+  } else {
+    // restart after ping so recover saved state
+    Serial.println("Reset after ping");
+    bot.sendMessage(CHAT_ID, "Reset after ping failure\n", "");
+    EEPROM.get(teehmode, hmode);
+    EEPROM.get(teehtemp, htemp);
+    EEPROM.get(teehzone, hzone);
+    EEPROM.get(teedst, dst);
+    EEPROM.get(teehyst, hysT);
+    EEPROM.get(teetotTime, totTime);
+    EEPROM.get(teelastReset, lastReset);
+  }
+
+  pRestart = 0;
+  EEPROM.put(teepRestart, pRestart);  // only writes to EEPROM if pRestart has changed 
+  EEPROM.commit(); 
+ 
+  pinMode(HEATER, OUTPUT);
+  digitalWrite(HEATER, HEATER_OFF);
+
+  sensors.begin();
+
+  // initialise 10 sec and 1 sec counters
+  last10 = now();
+  last1 = millis();
+  lastday = now();
+}
+
+
+void loop() {
+
+
+  if(now() - lastday >= 86400){
+    syncTime();
+    lastday = now();
+  }
+
+  if (now() - last10 >= TEMP_TIME) {
+    // every 10 secs check if heater needs to switch on
+    airTemp = getTemp();
+    uint8_t zone = getZone(now());
+    switch(hmode){
+      case 0 :
+        hstate = setHeater(false);
+      break;
+      case 1 :
+        hstate = setHeater(true);
+      break;
+      case 2 :{
+        if (airTemp < (float)(htemp[zone] - hysT)) {
+          hstate = setHeater(true);
+         }
+        if (airTemp > (float)(htemp[zone] + hysT)) {
+          hstate = setHeater(false);
+        }
+      }
+      break;
+      case 3:{
+        if(now() > boostTime){
+          hmode = lastmode;
+          hstate = setHeater(false);
+        } else
+          hstate = setHeater(true);
+        
+      }
+      break;
+    }
+    String text = "";
+    if(logging){
+      char sz[10];
+      sprintf(sz, "%02d:%02d:%02d", hour(), minute(), second());
+      text = String(sz) + " Air:- " + String(airTemp, 1);
+      text += " Req: " + String(htemp[zone]);
+      text += " Heater: ";  
+      if (hstate) {
+        text += "ON";
+      } else {
+        text += "OFF";
+      }
+      text += "\n";
+
+      bot.sendMessage(CHAT_ID, text, "");
+    }
+
+    // check if we are still connected to internet
+    if(! Ping.ping(ping_host, 1)) {
+      String xt = "Ping failed -";
+      if(WiFi.reconnect()){
+        xt += "Reconnecting to wiFi";   
+      } else {
+        xt += "Restarting WiFi";
+        WiFi.begin(ssid, password);
+      }
+      while(WiFi.waitForConnectResult() != WL_CONNECTED){
+        // save current state of variables
+        EEPROM.put(teehmode, hmode);
+        EEPROM.put(teehtemp, htemp);
+        EEPROM.put(teehzone, hzone);
+        EEPROM.put(teedst, dst);
+        EEPROM.put(teehyst, hysT);
+        if (hstate){
+          // heater is on so find out how long since timeoff 
+          totTime += (now() - onTime);
+        }
+        EEPROM.put(teetotTime, totTime);
+        pRestart = 1;
+        EEPROM.put(teepRestart, pRestart);
+        EEPROM.put(teelastReset, lastReset);
+        EEPROM.commit();
+        delay(2);
+        ESP.restart();
+      }
+      xt += ": OK\n";
+      bot.sendMessage(CHAT_ID, xt, "");
+    }
+
+    last10 = now();
+  }
+  if (millis() - last1 >= TELEGRAM_TIME) {  // check for new telegram commands every secon
+    int numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+    while (numNewMessages) {
+      handleNewMessages(numNewMessages);
+      numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+    }
+
+    
+    last1 = millis();
+  }
+}
---
+++ heaterlogic.h
@@ -0,0 +1,339 @@
+// START OF PROGRAM ********
+
+void syncTime(void) {
+  configTime(gmtOffset_sec, daylightOffset_sec, "time.google.com");
+  time_t now = time(nullptr);
+  while (now < SECS_YR_2000) {
+    delay(100);
+    now = time(nullptr);
+  }
+  if (dst) {
+    now += 3600;
+  }
+  setTime(now);
+}
+
+
+uint8_t getZone(time_t t) {
+
+  uint16_t dMin = hour(t) * 60 + minute();
+  for (int i = 2; i >= 0 ; i--) {
+    if (dMin - hzone[i] > 0) {
+      return (i + 1);
+    }
+  }
+  return (0);
+}
+
+String pTime(int T) {
+  char sz[6];
+
+  sprintf(sz, "%02d:%02d", T / 60, T % 60);
+  return (String(sz));
+}
+
+int gTime(char * sz) {
+  char s;
+  int t1, t2;
+  if (strlen(sz) < 4) {
+    return (-1);
+  }
+  sscanf(sz, "%d %c %d", &t1, &s, &t2);
+  return (t1 * 60 + t2);
+}
+
+
+void handleNewMessages(int numNewMessages) {
+  
+  static bool waitforYes = false;
+  String JSonCmd =  "[[\"On\", \"Off\", \"Auto\",\"Boost\"],[\"Status\",\"Help\"],[\"Temp\",\"Time\",\"Sync\",\"Hyst\"]]"; 
+  
+  Serial.print("Handle New Messages: ");
+  Serial.println(numNewMessages);
+
+  for (int i = 0; i < numNewMessages; i++) {
+    String chat_id = String(bot.messages[i].chat_id);
+    //    Serial.println(chat_id);
+    if (chat_id != CHAT_ID) {
+      bot.sendMessage(chat_id, "Unauthorized user", "");
+      continue;
+    }
+    String welcome = "Command not recognised";
+
+    // Print the received message
+    String text = bot.messages[i].text;
+    text.toLowerCase();
+    Serial.println(text);
+
+    if(waitforYes && (text == "yes")){
+      welcome = "Resetting...";
+      bot.sendMessage(CHAT_ID, welcome, "");
+      bot.getUpdates(bot.last_message_received + 1);
+      delay(1000);
+      ESP.restart();
+    }
+
+    waitforYes = false;
+    if (text == "reset"){
+      welcome = "Reset..Send Yes to confirm\n";
+      waitforYes = true; 
+    }
+    
+    if ((text == "help") || (text == "/start")) {
+      String from_name = bot.messages[i].from_name;
+      welcome = "Welcome , " + from_name + "\n";
+      welcome += "Menu: \n";
+      welcome += "Sync : Reset clock\n";
+      welcome += "Off : Heater OFF\n";
+      welcome += "On : Heater ON\n";
+      welcome += "Auto : Heater AUTO\n";
+      welcome += "Boost : Switch on for 1 hour\n";
+      welcome += "Status : Status \n";
+      welcome += "Temp : Show/Edit Zone temps \n";
+      welcome += "Time : Show/Edit Time zone boundaries \n";
+      welcome += "DST : toggles DST On/Off\n";
+      welcome += "Save : Saves params to EEPROM\n";
+
+    }
+
+    if(text == "boost"){
+      // switch on heater for one hour
+      boostTime = now() + 3600;
+      lastmode = hmode;
+      hmode = 3;
+      welcome = "Boost - heater on for 1 hour";    
+    }
+
+    if(text == "log") {
+      logging = !logging;
+      welcome = "Logging: ";
+      if (logging) {
+        welcome += "ON\n";
+      }
+      else {
+        welcome += "OFF\n";
+      }
+    }
+
+    if (text == "save") {
+      EEPROM.put(eehmode, hmode);
+      EEPROM.put(eehtemp, htemp);
+      EEPROM.put(eehzone, hzone);
+      EEPROM.put(eedst, dst);
+      EEPROM.put(eehyst, hysT);
+      EEPROM.commit();
+      welcome = "User params saved";
+    }
+    
+    if (text == "status") {
+      char timeStr[20];
+
+      sprintf(timeStr, "%02d:%02d:%02d", hour(), minute(), second());
+      welcome = "Status:   \n";
+      welcome += "Time: " + String(timeStr) + "\n";
+      welcome += "Mode: ";
+      switch (hmode) {
+        case 0:
+          welcome += "OFF\n";
+          break;
+        case 1:
+          welcome += "ON\n";
+          break;
+        case 2:
+          welcome += "AUTO\n";
+          break;
+        case 3:
+          welcome += "BOOST\n";
+      }
+      uint8_t gz = getZone(now());
+      welcome += "Zone: " + String(gz) + "\n";
+      welcome += "Req temp: " + String(htemp[gz]) + "\n";
+      welcome += "Air temp: "  + String(airTemp, 1) + "\n";
+      welcome += "Heater: ";
+      if (hstate) {
+        welcome += "ON\n";
+      }
+      else {
+        welcome += "OFF\n";
+      }
+      welcome += "DST: ";
+      if (dst) {
+        welcome += "ON\n";
+      } else {
+        welcome += "OFF\n";
+      }
+      char ton[20], tr[22];
+      unsigned int hr, mn, sc;
+      if (hstate){
+        // heater is on so find out how long since timeoff
+        totTime += (now() - onTime);
+        onTime = now();
+      }
+      hr = totTime / 3600;
+      mn = (totTime - hr * 3600) / 60;
+      sc = totTime - (hr * 3600) - (mn * 60);
+      sprintf(ton, "%d:%02d:%02d", hr, mn, sc);
+      sprintf(tr, "%d/%d/%d - %02d:%02d", day(lastReset), month(lastReset), year(lastReset), hour(lastReset), minute(lastReset));
+      welcome += "Heater on: " + String(ton) + " since " + String(tr) + "\n";
+    }
+
+    if (text == "sync") {
+      syncTime();
+      char timeStr[20];
+      sprintf(timeStr, "%02d:%02d:%02d", hour(), minute(), second());
+      welcome = "Time: " + String(timeStr) + "\n";
+    }
+
+    if (text == "off") {
+      hmode = 0;  
+      welcome = "Mode : OFF\n";
+    }
+    
+    if (text == "on") {
+      hmode = 1;
+      welcome = "Mode : ON\n";
+    }
+    
+    if (text == "auto") {
+      hmode = 2;
+      welcome = "Mode : AUTO\n";
+    }
+
+    if (text.startsWith("hyst")) {
+      // processing hysteresis setting
+      // hyst 0 - 5
+      char ch[30];
+      char *token;
+      int z, t;
+
+      text.toCharArray(ch, text.length() + 1);
+      token = strtok(ch, " ");
+      token = strtok(NULL, ",");
+      z = 0;
+      t = -1;
+      if (token != NULL) {
+        if (strlen(token) > 0){
+          sscanf(token, "%d", &t);
+        }
+        if((t >= 0) && (t <= 5)){
+          hysT = t;
+        }
+      }
+      welcome = "Hysteresis setting: " + String(hysT) + "\n";
+    }    
+
+    if (text.startsWith("temp")) {
+      // processing temps for each zone
+      // where temps are comma separatd list with 4 fields field = -1 if temp to be ignore
+      char ch[30];
+      char *token;
+      int z, t;
+
+      text.toCharArray(ch, text.length() + 1);
+      token = strtok(ch, " ");
+      token = strtok(NULL, ",");
+      z = 0;
+      t = 2;
+      while (token != NULL) {
+        t = -1;
+        if(strlen(token) > 0){
+          sscanf(token, "%d", &t);
+        }
+        if (t > 0) {
+          htemp[z] = (uint8_t)t;
+        }
+        z++;
+        token = strtok(NULL, ",");
+      }
+      welcome = "Temp settings: " + String(htemp[0]);
+      for (z = 1; z < 4; z++) {
+        welcome += ", " + String(htemp[z]);
+      }
+      welcome += "\n";
+    }
+
+    if (text.startsWith("time")) {
+      // processing zone times for each zone
+      // where times are comma separatd list with 4 fields field = -1 if time to be ignore
+      char ch[30];
+      char *token;
+      int z, t;
+
+      text.toCharArray(ch, text.length() + 1);
+      token = strtok(ch, " ");
+      token = strtok(NULL, ",");
+      z = 0;
+      t = 2;
+      while (token != NULL) {
+        t = gTime(token);
+        if (t > 0) {
+          hzone[z] = t;
+        }
+        z++;
+        token = strtok(NULL, ",");
+      }
+      welcome = "Zone boundaries: " + pTime(hzone[0]);
+      for (z = 1; z < 3; z++) {
+        welcome += ", " + pTime(hzone[z]);
+      }
+      welcome += "\n";
+    }
+
+    if (text == "dst") {
+      (dst) ? dst = false  : dst = true;
+      welcome = "DST : ";
+      if (dst) {
+        welcome += "ON\n";
+      } else {
+        welcome += "OFF\n";
+      }
+      syncTime();
+    }
+
+    bot.sendMessageWithReplyKeyboard(chat_id, welcome, "", JSonCmd, true);
+
+//    bot.sendMessage(CHAT_ID, welcome, "");
+
+  }
+}
+
+
+float getTemp(void) {
+
+  sensors.requestTemperatures(); 
+  float tempC = sensors.getTempCByIndex(0);
+  return (tempC);
+}
+
+bool setHeater(bool newState) {
+  
+  if(hstate != newState) {
+    // set GPIO to control radiator on/off
+    if(newState){
+      onTime = now();
+      offTime = onTime;
+      digitalWrite(HEATER, HEATER_ON);
+    }
+    else {
+      offTime = now();
+      if(offTime > onTime){
+        totTime += (offTime - onTime);
+        onTime = offTime;
+      }
+      digitalWrite(HEATER, HEATER_OFF);
+    }
+//    if(logging){
+      char sz[10];
+      sprintf(sz, "%02d:%02d:%02d", hour(), minute(), second());
+      String text = String(sz) + " Heater:- ";  
+      if (newState) {
+        text += "ON";
+      } else {
+        text += "OFF";
+      }
+      text += " at : " +String(airTemp, 1) + "\n";
+      bot.sendMessage(CHAT_ID, text, "");
+    }
+//  }
+  return ( newState );
+}
